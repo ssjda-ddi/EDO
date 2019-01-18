7e1d78f005180f009ae4d9da54f6cd933c70ce60
ssjda-ddi <ssjda-ddi@hotmail.com>
2019-01-18 17:09:35 +0900

    Commit EDO 2.0.0

---
+++ EDOTest/Core/IO/DDI3WriterTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EDO.Core.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDO.Core.IO.Tests
+{
+    [TestClass()]
+    public class DDI3WriterTests
+    {
+        [TestMethod()]
+        public void WriteStudyUnitTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ EDOTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EDOTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EDOTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e180b63e-755e-4d40-8872-a8160b132651")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
